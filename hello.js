52c101c2481f9e5b804d23458f89acc2b5cfc78f
Unknown <wbecerra1@gmail.com>
2018-03-05 18:07:31 +0200

    JS weaklyy typed variable definitions

--- hello.js
+++ hello.js
@@ -1,1 +1,9 @@
 console.log("hello World!");
+let test;
+console.log("test =", test, "test is a", typeof test);
+test = true;
+console.log("test =", test, "test is a", typeof test);
+test += 5;
+console.log("test =", test, "test is a", typeof test);
+test += "test";
+console.log("test =", test, "test is a", typeof test);

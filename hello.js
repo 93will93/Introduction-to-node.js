d10babc9f142b9949a1c43180afebda60ee96785
Unknown <wbecerra1@gmail.com>
2018-03-05 18:10:10 +0200

    JS Objects

--- hello.js
+++ hello.js
@@ -7,3 +7,13 @@ test += 5;
 console.log("test =", test, "test is a", typeof test);
 test += "test";
 console.log("test =", test, "test is a", typeof test);
+
+
+let person = {}; //An empty object
+person.name = "Joe";
+person.surname = "Soap";
+person.age = "20";
+person.isMale = true;
+person.fullname = person.name + " " + person.surname; //we can access the
+//variables using their names
+console.log(person);

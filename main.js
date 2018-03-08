5ecb6379a9b648fb9428890edf4340b7cedb95cd
Unknown <wbecerra1@gmail.com>
2018-03-08 19:19:00 +0200

    Using File System to read a file

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-console.log("Loading a module")
+console.log("Loading a module");
 // notice that require returns the whole script. Meaning the whole script runs before main.js can continue.
-let mod = require("./mod")
-console.log("Mod: ", mod)
-console.log("Done")
+let mod = require("./mod");
+console.log("Mod: ", mod);
+console.log("Done");
+
+
+// Now using the file System module 
+let fs = require("fs");
+// The file encoding type has to be specified otherwise the buffer is returned.
+fs.readFile("./ELEN4010.txt", 'utf-8', (err, data) => {
+    if (err) throw err;
+    
+    console.log(data);    
+})

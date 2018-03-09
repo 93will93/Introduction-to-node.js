7167db14cbb77ca9a43fe74b9c2d1c17c07614a1
Unknown <wbecerra1@gmail.com>
2018-03-09 21:18:55 +0200

    Routing

--- Express/index.js
+++ Express/index.js
@@ -7,5 +7,10 @@ app.get('/', function(req, res){
 
 });
 
+app.get('/about', function(req, res){
+        res.sendFile(path.join(__dirname, 'views', 'about.html'));
+
+});
+
 app.listen(3000);
 console.log("Express server running on port 3000");
